1857d06906e34395fdfd2893bd8a396fb92cf2ef
charlykso <charlykso141@gmail.com>
2022-07-13 10:01:28 +0000

    trying to fix task 0

--- print_str.c
+++ print_str.c
@@ -10,19 +10,10 @@
 int print_str(char *str)
 {
 	int count = 0;
-	int i, j;
-	char null[] = "(null)";
+	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == NULL)
-		{
-			for (j = 0; nill[j]; j++)
-			{
-				count += _putchar(nill[j]);
-				return (count);
-			}
-		}
 		count += _putchar(str[i]);
 	}
 	return (count);
